6292349b8d509d61f724b32c6588891a2cb1faa1
Karl <karlsmith@bouzou.com>
2017-01-12 22:34:40 -0500

    Changes interval code to use browser requestAnimationFrame and also fixes bug where F-pentamino template works only half the time

--- templates.js
+++ templates.js
@@ -12,14 +12,14 @@ function glider(pixels) {
 }
 
 function fPentamino(pixels) {
-    var p1 = {x: 93, y: 52, s1: "black", s2: "white"};
+    var p1 = {x: 93, y: 52, s1: "black", s2: "black"};
     pixels.addPixel(p1);
-    var p2 = {x: 93, y: 53, s1: "black", s2: "white"};
+    var p2 = {x: 93, y: 53, s1: "black", s2: "black"};
     pixels.addPixel(p2);
-    var p3 = {x: 92, y: 52, s1: "black", s2: "white"};
+    var p3 = {x: 92, y: 52, s1: "black", s2: "black"};
     pixels.addPixel(p3);
-    var p4 = {x: 93, y: 51, s1: "black", s2: "white"};
+    var p4 = {x: 93, y: 51, s1: "black", s2: "black"};
     pixels.addPixel(p4);
-    var p5 = {x: 94, y: 51, s1: "black", s2: "white"};
+    var p5 = {x: 94, y: 51, s1: "black", s2: "black"};
     pixels.addPixel(p5);
 }
